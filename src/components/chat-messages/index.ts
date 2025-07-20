e6eef8e4d1c481c080cccd65be3a5b9ba1d46b06
Juan gomen <gomen98.jg@gmail.com>
2025-07-19 22:50:06 -0600

    feat(chat): implement initial chat functionality
    This commit establishes the core functionality for the chatbot interaction.
    
    - Enables users to send messages via the input component.
    - Dynamically adds user and assistant messages to the chat log.
    - Implements auto-scrolling to keep the latest message in view.
    - Connects the input and message display components via custom events.

--- src/components/chat-messages/index.ts
+++ src/components/chat-messages/index.ts
@@ -3,12 +3,27 @@ import template from "./template.html?raw";
 import "@/components/chat-message"; // Importing the chat-message component to ensure it's registered
 import style from "./style.css?inline";
 
+type Sender = "user" | "assistant";
+
 class ChatMessages extends BaseComponent {
+  private resizeObserver: ResizeObserver | null = null;
   constructor() {
     super();
     if (!this.shadowRoot) {
       this.attachShadow({ mode: "open" });
     }
+
+    this.resizeObserver = new ResizeObserver(() => this.scrollToBottom());
+  }
+  protected override connectedCallback(): void {
+    super.connectedCallback();
+    console.log("ChatMessages component connected");
+    const $ul =
+      this.shadowRoot?.querySelector<HTMLUListElement>(".chat-messages");
+    if (!$ul) {
+      return;
+    }
+    this.resizeObserver?.observe($ul);
   }
 
   protected override get htmlTemplate(): string {
@@ -20,7 +35,71 @@ class ChatMessages extends BaseComponent {
   }
 
   protected override setupEventListeners(): void {
-    console.log("ChatMessages component initialized");
+    document.addEventListener("message-sent", (event) => {
+      const customEvent = event as CustomEvent<{ text: string }>;
+      const message = customEvent.detail.text;
+
+      this.addMessage(message, "user");
+      setTimeout(() => {
+        this.addMessage("Hi there", "assistant");
+      }, 1000);
+    });
+  }
+
+  public addMessage(message: string, sender: Sender): void {
+    if (this.shadowRoot === null) {
+      return;
+    }
+
+    const $chatMessageElement = document.createElement("chat-message");
+    $chatMessageElement.dataset.sender = sender;
+    const $template = this.shadowRoot.querySelector<HTMLTemplateElement>(
+      "#user-message-template",
+    );
+    const $ul =
+      this.shadowRoot.querySelector<HTMLUListElement>(".chat-messages");
+
+    if (!$template) {
+      return;
+    }
+
+    const $clone = $template.content.cloneNode(true) as DocumentFragment;
+    // const $clone = $template.content.cloneNode(true);
+    const $paragraph = $clone.querySelector("p");
+    const $sender = $clone.querySelector("span");
+    if (!$paragraph) {
+      return;
+    }
+
+    $paragraph.textContent = message;
+    if ($sender) {
+      $sender.textContent = sender === "user" ? "Y" : "A";
+    }
+
+    $chatMessageElement.appendChild($clone);
+
+    if (!$ul) {
+      return;
+    }
+
+    $ul.appendChild($chatMessageElement);
+
+    // this.scrollToBottom();
+  }
+
+  public scrollToBottom(): void {
+    if (this.shadowRoot === null) {
+      return;
+    }
+
+    const rootNode = this.shadowRoot.getRootNode() as ShadowRoot;
+    const host = rootNode.host;
+    host.scrollTop = host.scrollHeight; // Scroll to the bottom of the chat messages
+    console.log(host);
+  }
+
+  protected override disconnectedCallback(): void {
+    this.resizeObserver?.disconnect();
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -20,28 +20,28 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 `;
 
 // Wait for the DOM to be fully loaded before running the script
-// window.addEventListener("DOMContentLoaded", () => {
-//   // Check if the visualViewport API is supported
-//   if (window.visualViewport) {
-//     const mainContent =
-//       document.querySelector<HTMLHtmlElement>(".main-content");
-//
-//     if (!mainContent) {
-//       return;
-//     }
-//
-//     // This function sets the container's height to the visible area's height
-//     const handleViewportResize = () => {
-//       // Get the current height of the visible area
-//       const viewportHeight = window?.visualViewport?.height;
-//       // Apply this height directly to the main content element
-//       mainContent.style.height = `${viewportHeight}px`;
-//     };
-//
-//     // Run the function once on load
-//     handleViewportResize();
-//
-//     // Add an event listener to run the function whenever the viewport resizes
-//     window.visualViewport.addEventListener("resize", handleViewportResize);
-//   }
-// });
+window.addEventListener("DOMContentLoaded", () => {
+  // Check if the visualViewport API is supported
+  if (window.visualViewport) {
+    const mainContent =
+      document.querySelector<HTMLHtmlElement>(".main-content");
+
+    if (!mainContent) {
+      return;
+    }
+
+    // This function sets the container's height to the visible area's height
+    const handleViewportResize = () => {
+      // Get the current height of the visible area
+      const viewportHeight = window?.visualViewport?.height;
+      // Apply this height directly to the main content element
+      mainContent.style.height = `${viewportHeight}px`;
+    };
+
+    // Run the function once on load
+    handleViewportResize();
+
+    // Add an event listener to run the function whenever the viewport resizes
+    window.visualViewport.addEventListener("resize", handleViewportResize);
+  }
+});
